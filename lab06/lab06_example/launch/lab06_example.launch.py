444c401d88491cb28ae54fcfe38465117a7d51f3
Tianchi Chen <mithrandir_chen@hotmail.com>
2025-11-14 11:21:40 +0000

    lab06 release

---
+++ lab06/lab06_example/launch/lab06_example.launch.py
@@ -0,0 +1,61 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command, PathJoinSubstitution
+from launch.substitutions import FindExecutable
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description() -> LaunchDescription:
+    robot_description_content = Command([
+        PathJoinSubstitution([
+            FindExecutable(name='xacro')
+        ]),
+        ' ',
+        PathJoinSubstitution([
+            FindPackageShare('youbot_description'),
+            'robots',
+            'youbot_arm_only.urdf.xacro'
+        ])
+    ])
+
+    robot_description = {
+        'robot_description': ParameterValue(
+            robot_description_content,
+            value_type=str
+        )
+    }
+
+    rviz_config = PathJoinSubstitution([
+        FindPackageShare('youbot_simulator'),
+        'config',
+        'youbot.rviz'
+    ])
+
+    return LaunchDescription([
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            parameters=[{'use_gui': True}]
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[robot_description]
+        ),
+        Node(
+            package='lab06_example',
+            executable='lab06_example',
+            name='youbot_kdl_node',
+            parameters=[robot_description]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz',
+            arguments=['-d', rviz_config],
+            output='screen'
+        )
+    ])
